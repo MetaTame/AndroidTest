63526508b103c36a73c1f3b67b0f5d28f0738749
Michael Ebbage <michael.ebbage@googlemail.com>
2013-08-20 14:19:56 +0100

    Link to new activity (via Intent), but stops application.

--- src/com/olamic/androidtest/MainActivity.java
+++ src/com/olamic/androidtest/MainActivity.java
@@ -2,7 +2,9 @@ package com.olamic.androidtest;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
 
 public class MainActivity extends Activity {
 
@@ -18,6 +20,12 @@ public class MainActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
+	}
+	
+	/** Called when the user clicks the Send button */
+	public void sendMessage(View view) {
+		Intent intent = new Intent(MainActivity.this, ShotMeter.class);
+	    startActivity(intent);
 	}
 
 }
